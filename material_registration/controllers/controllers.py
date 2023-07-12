9eb1509b1bce0bb118899b4d2d52d34af81a88f0
kennyric <kennyricardo1007@gmail.com>
2023-07-12 20:17:58 +0700

    update controllers

--- material_registration/controllers/controllers.py
+++ material_registration/controllers/controllers.py
@@ -4,13 +4,18 @@ from odoo.exceptions import UserError, ValidationError
 
 class MaterialController(http.Controller):
 
-    @http.route('/material/create', type='http', auth='user', website=False)
+    #Create
+    @http.route('/material/create', type='json', auth='user')
     def create_material(self, **post):
         code = post.get('code')
         name = post.get('name')
         material_type = post.get('type')
         buy_price = float(post.get('buy_price'))
-        supplier_id = int(post.get('supplier_id'))
+        supplier_id = post.get('supplier_id')
+        print(supplier_id)
+
+        if buy_price < 100:
+            return "Harga tidak boleh kurang dari 100!"
 
         if code and name and material_type and buy_price and supplier_id:
             material = request.env['material'].create({
@@ -21,32 +26,78 @@ class MaterialController(http.Controller):
                 'supplier_id': supplier_id
             })
 
-            # Menampilkan pesan sukses atau error
             if material:
-                return "Material created successfully."
+                return "Material berhasil didaftarkan."
             else:
-                return "Failed to create material."
+                return "Material gagal didaftarkan."
         else:
-            return "Failed to create material."
+            return "Material gagal didaftarkan."
 
-    @http.route('/material/update/<int:material_id>', type='http', auth='user', website=False)
+    #Update
+    @http.route('/material/update/<int:material_id>', type='json', auth="user")
     def update_material(self, material_id, **post):
         material = request.env['material'].browse(material_id)
-
         material.write(post)
 
-        return "Material updated successfully."
+        return "Material berhasil didaftarkan"
 
-    @http.route('/material/delete/<int:material_id>', type='http', auth='user', website=False)
+    #Delete
+    @http.route('/material/delete/<int:material_id>', type='json', auth='user')
     def delete_material(self, material_id):
         material = request.env['material'].browse(material_id)
         material.unlink()
 
-        return "Material deleted successfully."
+        return "Material berhasil dihapus."
 
-    @http.route('/material', type='http', auth='user', website=True)
-    def view_materials(self, **kw):
+    #Read
+    @http.route('/get_material', type='http', auth='user')
+    def get_materials(self, **kw):
         materials = request.env['material'].search([])
-        # return materials
+        items = []
+        for line in materials:
+            item = {
+                'id': line.id,
+                'code': line.code,
+                'name': line.name,
+                'type': line.type,
+                'buy_price': line.buy_price,
+                'supplier_id': line.supplier_id.id
+            }
+            items.append(item)
+        return str(items)
+
+    #get with filter
+    @http.route('/get_material/<string:material_type>', type='http', auth='user')
+    def get_materials_with_filter(self, material_type):
+        materials = request.env['material'].search([('type','=',material_type)])
+        items = []
+        for line in materials:
+            item = {
+                'id': line.id,
+                'code': line.code,
+                'name': line.name,
+                'type': line.type,
+                'buy_price': line.buy_price,
+                'supplier_id': line.supplier_id.id
+            }
+            items.append(item)
+        return str(items)
+
+    #Read returning view
+    @http.route('/get_material_view', type='http', auth='user')
+    def get_materials_view(self, **kw):
+        materials = request.env['material'].search([])
+        items = []
+        for line in materials:
+            item = {
+                'id': line.id,
+                'code': line.code,
+                'name': line.name,
+                'type': line.type,
+                'buy_price': line.buy_price,
+                'supplier_id': line.supplier_id.id
+            }
+            items.append(item)
 
         return request.render('material_registration.material_template', {'materials': materials})
+
